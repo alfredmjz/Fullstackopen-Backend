cab29c0d95178ef0dec8ac300516f0e99ec4453d
alfredmjz <alfredmjz@gmail.com>
2022-06-16 18:57:23 -0400

    Update db name

--- people.js
+++ people.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 
 mongoose
-	.connect(url)
+	.connect(url, { dbName: "Phonebook" })
 	.then(() => {
 		console.log("connected to MongoDB");
 	})
